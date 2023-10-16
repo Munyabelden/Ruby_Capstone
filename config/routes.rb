13b0e955d8b4d89aa47af64b72591c7924df1bef
munyamugb <munyaradzi045@gmail.com>
2023-10-16 17:32:25 +0200

    Connect database

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :foods, only: [:index, :show, :create, :update, :destroy]
+      resources :recipes, only: [:index, :show, :create, :update, :destroy]
+      resources :users, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
 end
+  
